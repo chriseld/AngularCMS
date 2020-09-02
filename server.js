eda533ce0baaf26e24f357a596f599a336140185
Chris Eld <chris@chriseld.com>
2020-09-02 16:19:41 -0400

    set up for heroku deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const mysql = require('mysql');
 
 app.use(express.static('public'))
